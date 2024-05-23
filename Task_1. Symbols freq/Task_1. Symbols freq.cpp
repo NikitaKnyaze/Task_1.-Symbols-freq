8cb99866fe410b9bb7d6b7fbcab15266c1ee7860
Knyazev Nikita <nk8222138@gmail.com>
2024-05-24 01:12:56 +0300

    first commit

---
+++ Task_1. Symbols freq/Task_1. Symbols freq.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+#include <map>
+#include <algorithm>
+#include <vector>
+
+int main()
+{
+	std::string str { "Hello world!!" };
+	std::map<char, int> vec;
+
+	for (const char& c : str)
+		++vec[c];
+	
+	std::vector<std::pair<char, int>> sortedVec(vec.begin(), vec.end());
+
+	std::sort(sortedVec.begin(), sortedVec.end(), [](const std::pair<int, char>& a, const std::pair<int, char>& b) {
+		return a.second > b.second;
+		});
+
+	for (const auto& pair : sortedVec)
+	{
+		std::cout << pair.first << ": " << pair.second << std::endl;
+	}
+
+	return 0;
+}
